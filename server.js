f414931d754c83ec2dc816cdc44352c899c466aa
bernabasa <75853223+bernabasa@users.noreply.github.com>
2023-02-16 13:52:04 -0500

    checking sql response

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
+const mysql = require('mysql2');
+
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 
 
 
-// Express middleware
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+// GET  a single candidate
+db.query(`SELECT * FROM candidates WHERE id =1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to databsae
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'bernabas abdeta',
+      // Your MySQL password
+      password: 'Workhappy2023$',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 // Default response for any other request (NOT FOUND)
 app.use((req, res) => {
