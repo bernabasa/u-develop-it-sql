12c3168c8f6375d52531a12baea542fc540b8954
bernabasa <75853223+bernabasa@users.noreply.github.com>
2023-02-20 16:37:09 -0500

    crud in progress

--- server.js
+++ server.js
@@ -11,23 +11,40 @@ const app = express();
 // db.query(`SELECT * FROM candidates`, (err, rows) => {
 //   console.log(rows);
 // });
-// GET  a single candidate
-db.query(`SELECT * FROM candidates WHERE id =1`, (err, row) => {
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
   if (err) {
     console.log(err);
   }
   console.log(row);
 });
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connectied)
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+})
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Connect to databsae
-const db = mysql.createConnection(
+const db = mysql.createConnection (
     {
       host: 'localhost',
       // Your MySQL username,
-      user: 'bernabas abdeta',
+      user: 'bernabas.abdeta@yahoo.com',
       // Your MySQL password
       password: 'Workhappy2023$',
       database: 'election'
